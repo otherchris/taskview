dd73ed5442ad152afad96f6f649948c5ee2f153b
Chris Caragianis <chriscaragianis@gmail.com>
2025-06-21 16:07:53 -0400

    refactor: Update tests for home and tasks controllers; change home index URL to root URL, enhance task creation test to assert task count and redirection, and modify home page test to include task creation functionality.

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,8 +1,11 @@
 require "test_helper"
 
 class TasksControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get tasks_create_url
-    assert_response :success
+  test "should create task" do
+    assert_difference("Task.count") do
+      post tasks_url, params: {task: {name: "Test Task", duration_in_seconds: 60}}
+    end
+
+    assert_redirected_to root_url
   end
 end
--- test/system/home_pages_test.rb
+++ test/system/home_pages_test.rb
@@ -1,9 +1,15 @@
 require "application_system_test_case"
 
 class HomePagesTest < ApplicationSystemTestCase
-  test "visiting the index" do
+  test "visiting the index and creating a task" do
     visit root_url
 
-    assert_selector "strong", text: "Rails"
+    fill_in "Name", with: "Test Task"
+    fill_in "Minutes", with: "10"
+
+    click_on "Create Task"
+
+    assert_text "Test Task"
+    assert_text "10 minutes remaining"
   end
 end
