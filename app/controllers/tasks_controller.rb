7e66758f7f6b8c62fc240508c6fd1f4a7ae4a14f
Chris Caragianis <chriscaragianis@gmail.com>
2025-06-21 14:07:09 -0400

    chore: Clean up formatting in Gemfile, CI configuration, and environment files; update task controller for consistent string usage; modify routes for syntax consistency; enhance database schema index formatting.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,18 @@
 class TasksController < ApplicationController
   def create
     duration = (params[:task][:years].to_i * 365 * 24 * 60 * 60) +
-               (params[:task][:months].to_i * 30 * 24 * 60 * 60) +
-               (params[:task][:weeks].to_i * 7 * 24 * 60 * 60) +
-               (params[:task][:days].to_i * 24 * 60 * 60) +
-               (params[:task][:hours].to_i * 60 * 60) +
-               (params[:task][:minutes].to_i * 60)
+      (params[:task][:months].to_i * 30 * 24 * 60 * 60) +
+      (params[:task][:weeks].to_i * 7 * 24 * 60 * 60) +
+      (params[:task][:days].to_i * 24 * 60 * 60) +
+      (params[:task][:hours].to_i * 60 * 60) +
+      (params[:task][:minutes].to_i * 60)
 
     @task = Task.new(name: params[:task][:name], duration_in_seconds: duration)
 
     if @task.save
       redirect_to root_path, notice: "Task was successfully created."
     else
-      render 'home/index', status: :unprocessable_entity
+      render "home/index", status: :unprocessable_entity
     end
   end
 
